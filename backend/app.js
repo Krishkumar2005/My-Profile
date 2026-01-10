23f0192fa25b9a8dd80da701f980714181f50729
Krish Kumar <119742219+Krishkumar2005@users.noreply.github.com>
2026-01-10 23:02:01 +0530

    Update CORS origin to allow multiple domains

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,9 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+             https://my-profile-sooty-ten.vercel.app/
+    ]
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
