20f2b03307839f3e6436276909bd870405db57db
Krish Kumar <119742219+Krishkumar2005@users.noreply.github.com>
2026-01-11 01:03:32 +0530

    Implement rate limiting and helmet for security
    Added rate limiting and security middleware to the app.

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,19 @@
 import express from "express"
 import cors from "cors"
+import {rateLimit} from "express-rate-limit"
+import helmet from "helmet";
 import healthRoute from "./routes/health.js"
 import userProfileRouter from "./routes/profile.js"
 import userProfileQueryRouter from "./routes/profileQuery.js"
 
 const app = express();
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	message: "Too many request performed from this IP, please try agin after some time!!"
+})
+
 app.use(express.json())
 
 // app.use(cors({
@@ -23,6 +32,9 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
+app.use(helmet())
+app.use(limiter)
 
 app.use("/api/v1", healthRoute)
 app.use("/api/v1", userProfileRouter)
